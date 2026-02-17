7148e66dc0596f81487469fee819a87185cfb3d0
Sukanya262006 <sukanyabudhiraja26@gmail.com>
2026-02-17 08:56:24 +0530

    Add DHT11 sensor support and update platformio configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <Arduino.h>
-void setup() {
+#include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+void setup(){
+    Serial.begin(9600);
+    dht.begin();
+    Serial.println("DHT11 Sensor Initialized");
+}
 
 
 void loop() {
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+    if (isnan(humidity) || isnan(temperature))
+    {
+        Serial.println("Failed to read from DHT sensor!");
+        delay(2000);
+        return;
+    }
+    Serial.println("Humidity: ");
+    Serial.println(humidity);
+    Serial.print(" % | ");
+    Serial.println("Temperature: ");
+    Serial.println(temperature);
+    Serial.println( " *C");
+    delay(2000);
 
 }
